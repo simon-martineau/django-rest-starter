5775bfb9f0920312fba26c1197f4a5dac853e4ac
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-22 20:00:44 -0500

    Prod config change

--- src/django_rest_starter/settings/production.py
+++ src/django_rest_starter/settings/production.py
@@ -16,14 +16,19 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
-# DATABASE
-# --------------------------------------------------------------------------------
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.environ['DATABASE_URL'],
-}
-
-
 # https://docs.djangoproject.com/en/3.1/ref/settings/#secure-proxy-ssl-header
 # This tells django that requests with this header are secure (https is handled by the proxy)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# DATABASE
+# --------------------------------------------------------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DATABASE_URL'],
+    }
+}
+
+# STATIC
+# --------------------------------------------------------------------------------
+STATIC_ROOT = 'staticfiles'
