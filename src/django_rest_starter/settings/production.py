1a67723de2f79a58933d0d01c17519f9db6f2443
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-22 22:09:02 -0500

    Added s3

--- src/django_rest_starter/settings/production.py
+++ src/django_rest_starter/settings/production.py
@@ -6,6 +6,12 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = False
 ALLOWED_HOSTS = ['starter.simonmartineau.dev', ]
 
+# APPS
+# --------------------------------------------------------------------------------
+INSTALLED_APPS += [
+    'storages'
+]
+
 # REST FRAMEWORK
 # --------------------------------------------------------------------------------
 REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = ['rest_framework.renderers.JSONRenderer', ]
@@ -32,3 +38,19 @@ DATABASES = {
 # STATIC
 # --------------------------------------------------------------------------------
 STATIC_ROOT = 'staticfiles'
+
+# AWS
+# --------------------------------------------------------------------------------
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'drs/static'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_DIRS = []
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
