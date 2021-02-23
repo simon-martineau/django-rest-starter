e5cf9e4732656e7ee33788d0909dd267e5b62c1a
Simon Martineau <simon.martineau.3@ulaval.ca>
2021-02-22 19:56:37 -0500

    Prod config change

--- src/django_rest_starter/settings/base.py
+++ src/django_rest_starter/settings/base.py
@@ -17,15 +17,6 @@ USE_TZ = True
 
 # DATABASES
 # ------------------------------------------------------------------------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ['POSTGRES_HOST'],
-        'NAME': os.environ['POSTGRES_DB'],
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-    }
-}
 
 # URLS
 # ------------------------------------------------------------------------------------------
--- src/django_rest_starter/settings/development.py
+++ src/django_rest_starter/settings/development.py
@@ -16,3 +16,13 @@ INTERNAL_IPS = [
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = '/var/www/static'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ['POSTGRES_HOST'],
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+    }
+}
